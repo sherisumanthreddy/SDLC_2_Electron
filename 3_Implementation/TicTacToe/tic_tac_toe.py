9d6cd09614c57826bb4077ac41c64d5d64d1e6fb
Prajwal-M1804 <80813102+Prajwal-M1804@users.noreply.github.com>
2021-08-21 18:31:11 +0530

    Update tic_tac_toe.py

--- 3_Implementation/TicTacToe/tic_tac_toe.py
+++ 3_Implementation/TicTacToe/tic_tac_toe.py
@@ -1,20 +1,27 @@
+"""import random module"""
+import random
 board = [' ' for x in range(10)]
 
-def insert_letter(letter,pos):
+def insert_letter(letter, pos):
+    """Function to insert letter at particular position"""
     board[pos] = letter
 
 def take_input():
+    """Function to take input from user whether to play or not"""
     opt = input("Do you want to play? (y/n) : ")
     return opt
 
 def take_move():
+    """Function to select position on board by choosing numbers from 1 to 9"""
     move = input("Select a position to enter the X between 1 to 9 : ")
     return move
 
 def space_is_free(pos):
+    """Function to check whether space is free or not"""
     return board[pos] == ' '
 
 def print_board(board):
+    """Function to design tic-tac-toe board"""
     print('   |   |   ')
     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
     print('   |   |   ')
@@ -24,32 +31,35 @@ def print_board(board):
     print('   |   |   ')
     print('------------')
     print('   |   |   ')
-    print(' ' + board[7]  + ' | ' + board[8] + ' | ' + board[9])
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
     print('   |   |   ')
 
 def is_board_full(board):
+    """Function to check  board is full"""
     if board.count(' ') != 1:
         return False
     else:
         return True
 
-def is_winner(b,l):
-    return ((b[1] == l and b[2] == l and b[3] == l) or
-    (b[4] == l and b[5] == l and b[6] == l) or
-    (b[7] == l and b[8] == l and b[9] == l) or
-    (b[1] == l and b[4] == l and b[7] == l) or
-    (b[2] == l and b[5] == l and b[8] == l) or
-    (b[3] == l and b[6] == l and b[9] == l) or
-    (b[1] == l and b[5] == l and b[9] == l) or
-    (b[3] == l and b[5] == l and b[7] == l))
+def is_winner(b, ch):
+    """Function for checking winner"""
+    return ((b[1] == ch and b[2] == ch and b[3] == ch) or
+            (b[4] == ch and b[5] == ch and b[6] == ch) or
+            (b[7] == ch and b[8] == ch and b[9] == ch) or
+            (b[1] == ch and b[4] == ch and b[7] == ch) or
+            (b[2] == ch and b[5] == ch and b[8] == ch) or
+            (b[3] == ch and b[6] == ch and b[9] == ch) or
+            (b[1] == ch and b[5] == ch and b[9] == ch) or
+            (b[3] == ch and b[5] == ch and b[7] == ch))
 
 def player_move():
+    """Function for player move"""
     run = True
     while run:
         move = take_move()
         try:
             move = int(move)
-            if move > 0 and move < 10:
+            if 0 < move < 10:
                 if space_is_free(move):
                     run = False
                     insert_letter('X', move)
@@ -62,6 +72,7 @@ def player_move():
             print('Please type a number')
 
 def computer_move():
+    """Function for computer move"""
     possible_moves = [x for x, letter in enumerate(board) if letter == ' ' and x != 0]
     move = 0
 
@@ -88,26 +99,27 @@ def computer_move():
 
     edges_open = []
     for i in possible_moves:
-        if i in [2,4,6,8]:
+        if i in [2, 4, 6, 8]:
             edges_open.append(i)
 
     if len(edges_open) > 0:
         move = select_random(edges_open)
         return move
 
-def select_random(li):
-    import random
-    ln = len(li)
-    r = random.randrange(0,ln)
-    return li[r]
+def select_random(list_num):
+    """Function to select random number"""
+    length = len(list_num)
+    r_int = random.randrange(0, length)
+    return list_num[r_int]
 
 def main():
+    """Function of main program"""
     print("Welcome to tic-tac-toe game!")
     print_board(board)
     if is_board_full(board):
         print("Tie game")
-    while not(is_board_full(board)):
-        if not(is_winner(board, 'O')):
+    while not (is_board_full(board)):
+        if not (is_winner(board, 'O')):
             player_move()
             print_board(board)
             if is_board_full(board):
@@ -117,7 +129,7 @@ def main():
             print("You loose! try again")
             break
 
-        if not(is_winner(board, 'X')):
+        if not (is_winner(board, 'X')):
             move = computer_move()
             if move == 0:
                 print(" ")
