7d6e33374baa479d9d3664ba13107b2fecee5638
Swathi Ramachandran <66193894+Swathir1@users.noreply.github.com>
2021-08-21 23:07:14 +0530

    Update check.py

--- 3_Implementation/TicTacToe/check.py
+++ 3_Implementation/TicTacToe/check.py
@@ -4,40 +4,41 @@ import printboard
 # Tells us who the current player is (X goes first)
 first_player = "X"
 
+
 # Handle a turn for an arbitrary player
 def manage_turn(player):
+    # Get position from player
+    print(player + " has next move.")
 
-  # Get position from player
-  print(player + " has next move.")
-  
-  place = input("Choose a place from 1-9: ")
+    place = input("Choose a place from 1-9: ")
 
-  # Whatever the user inputs, make sure it is a valid input, and the spot is open
-  
-  validity = False
-  
-  while not validity:
+    # Whatever the user inputs, make sure it is a valid input, and the spot is open
 
-    # Make sure the input is valid
-    
-    while place not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
-      place = input("Choose a place from 1-9: ")
- 
-    # Get correct index in our board list
-    
-    place = int(place) - 1
+    validity = False
 
-    # Then also make sure the spot is available on the board
-    
-    if printboard.play_board[place] == "-":
-      
-      validity = True
-    else:
-      print("wrong input please try again")
+    while not validity:
 
-  # Put the game piece on the board
-  
-  printboard.play_board[place] = player
+        # Make sure the input is valid
 
-  # Show the game board
-  printboard.print_board()
+        while place not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+            print("Wrong input. Try again")
+            place = input("Choose a place from 1-9: ")
+
+        # Get correct index in our board list
+
+        place = int(place) - 1
+
+        # Then also make sure the spot is available on the board
+
+        if printboard.play_board[place] == "-":
+            validity = True
+        elif printboard.play_board[place] != "-":
+            print("This space is occupied")
+            place = input("Choose a place from 1-9: ")
+
+    # Put the game piece on the board
+
+    printboard.play_board[place] = player
+
+    # Show the game board
+    printboard.print_board()
