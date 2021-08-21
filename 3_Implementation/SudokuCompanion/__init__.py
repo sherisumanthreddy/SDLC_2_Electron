63a6b160a1879a7ee619a69c7e885a3d706db779
Annappa259804 <80380749+Annappa259804@users.noreply.github.com>
2021-08-21 12:52:20 +0530

    Validity Function

--- 3_Implementation/SudokuCompanion/__init__.py
+++ 3_Implementation/SudokuCompanion/__init__.py
@@ -3,3 +3,16 @@ class SudokuSolver:
         self.sudoku_grid = sudoku_grid
         self.solution_grid = None
         self.count = 0
+
+    def is_valid_entry(self, row, col, entry):
+        for i in range(9):
+            if self.sudoku_grid.grid[row][i] == entry:
+                return False
+        for i in range(9):
+            if self.sudoku_grid.grid[i][col] == entry:
+                return False
+        for i in range(3):
+            for j in range(3):
+                if self.sudoku_grid.grid[(row//3)*3 + i][(col//3)*3 + j] == entry:
+                    return False
+        return True
