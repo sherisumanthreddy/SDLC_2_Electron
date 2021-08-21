3648a81b24df8e77b2c0294f2d967d4e3562ef0b
Bhargav R <40035575+bgvmysore@users.noreply.github.com>
2021-08-21 19:30:51 +0530

    test name change sudoku

--- 3_Implementation/SudokuCompanion/test_SudokuCompanion.py
+++ 3_Implementation/SudokuCompanion/test_SudokuCompanion.py
@@ -8,13 +8,13 @@ def test_FileIOHandler_ReadSudokuFromTxt():
     assert IO_Obj.ReadSudokuFromTxt(path_)
 
 
-def test_CleanReadData():
+def test_FileIOHandler_CleanReadData():
     var = "123\n4343,444###,##331231\n"
     var2 = "qwertyuiop"
     assert FileIOHandler.CleanReadData(var) == "1234343444.....331231"
     assert FileIOHandler.CleanReadData(var2) == ".........."
 
-def test_ValidateCleanedData():
+def test_FileIOHandler_ValidateCleanedData():
     var = "9..21.4...32.4.....4.8....7..7.5...9..17.23..3...8.17.5....9.8.....2854...8.....1"
     var2 = "9..21.4...32.4..A..4.8....7..7.5...9..17.23..3...8.17.5....9.8.....2854...8.....1"
     var3 = "9..21.4...32.4.....4.8....7..7.5...9..17.23..3...8.17.5....9.8.....2854...8.."
@@ -22,7 +22,7 @@ def test_ValidateCleanedData():
     assert not FileIOHandler.ValidateCleanedData(var2)
     assert not FileIOHandler.ValidateCleanedData(var3)
 
-def test_SaveSudokuToTxt():
+def test_FileIOHandler_SaveSudokuToTxt():
     path_ = "save.txt"
 
     var = "9..21.4...32.4.....4.8....7..7.5...9..17.23..3...8.17.5....9.8.....2854...8.....1"
@@ -32,4 +32,3 @@ def test_SaveSudokuToTxt():
     assert IO_Obj.ReadSudokuFromTxt(path_)
     
     os.remove(path_)
-
