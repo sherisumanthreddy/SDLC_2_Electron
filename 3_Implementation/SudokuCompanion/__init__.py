86dcf42085246156f5f47906263f858bf22fb499
Annappa259804 <80380749+Annappa259804@users.noreply.github.com>
2021-08-21 12:54:01 +0530

    Helpe Function added

--- 3_Implementation/SudokuCompanion/__init__.py
+++ 3_Implementation/SudokuCompanion/__init__.py
@@ -16,3 +16,17 @@ class SudokuSolver:
                 if self.sudoku_grid.grid[(row//3)*3 + i][(col//3)*3 + j] == entry:
                     return False
         return True
+
+    def helper_solve(self):
+        for i in range(0, 9):
+            for j in range(0, 9):
+                if self.sudoku_grid.grid[i][j] == '#':
+                    for val in range(1, 10):
+                        if self.is_valid_entry(i, j, val):
+                            self.sudoku_grid.grid[i][j] = val
+                            self.solve()
+                            self.sudoku_grid.grid[i][j] = '#'
+                    return
+        if self.count == 0:
+            self.solution_grid = deepcopy(self.sudoku_grid)
+            self.count += 1
