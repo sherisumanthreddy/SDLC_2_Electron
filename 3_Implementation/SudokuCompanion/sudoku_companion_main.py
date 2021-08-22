582c992a997a0f6ba6d6167327061e84a02a7e4d
Bhargav R <40035575+bgvmysore@users.noreply.github.com>
2021-08-22 22:51:53 +0530

    done with all cases sudoku console front

--- 3_Implementation/SudokuCompanion/sudoku_companion_main.py
+++ 3_Implementation/SudokuCompanion/sudoku_companion_main.py
@@ -1,5 +1,23 @@
 from SudokuCompanion import SudokuGrid, SudokuSolver, FileIOHandler
 
+def JustPrintSudoku(SudokuStr):
+        stringObj = SudokuStr
+        if not FileIOHandler.ValidateCleanedData(stringObj):
+            print("MegaError!!!!!!!!")
+        
+        print("-"*25)
+        for i in range(9):
+            print("|", end = " ")
+            for j in range(9):
+                print(stringObj[j+i*9], end=" ")
+                if (j+1)%3 == 0:
+                    print("|", end=" ")
+            print("\n", end="")
+            if (i+1)%3 == 0:
+                print("-"*25)
+                
+        return
+
 def ConsoleFront():
     path_to_puzzle = input("Enter the path to txt file containing the puzzle: ")
     GridObj = SudokuGrid(FileIOHandler, path_to_puzzle)
@@ -21,9 +39,9 @@ def ConsoleFront():
     gotNotChoice = True
 
     while gotNotChoice:
-        choice = input("Following Options are available:\n 1. Check if puzzle has a soln?\n 2. Provide Hint\n 3. Solve Puzzle\nEnter Option [1,2,3]: ")
+        choice = input("Following Options are available:\n 1. Check if puzzle has a soln?\n 2. Provide Hint\n 3. Solve Puzzle\nEnter Option [1,2,3] or q to quit: ")
 
-        if choice in ["1", "2", "3"]:
+        if choice in ["1", "2", "3", "q"]:
             gotNotChoice = False
         else:
             print()
@@ -33,16 +51,41 @@ def ConsoleFront():
             print()
     
     SolverObj = SudokuSolver(GridObj)
+    
     if choice == "1":
         value = "" if SolverObj.IsSolvable() else "NOT "
         print(f"The given sudoku puzzle can {value}be solved!")
+    
     elif choice == "2":
-        pass
+        hintStr = SolverObj.GetHintString()
+        print("HINT:")
+        JustPrintSudoku(hintStr)
+
     elif choice == "3":
-        pass
+        print("SOLUTION:")
+        answer = SolverObj.Solve()
+        answer.PrintSudoku()
+
+        wanna_save = input("Do you want to save solution to a file? [y/N]")
+        if wanna_save == 'y':
+            error = True
+            while error:
+                output_file = input("Enter output filename: ")
+                try:
+                    answer.setIODestination(output_file)
+                    if answer.Save():
+                        print("File Saved Succesfully!")
+                except FileExistsError:
+                    print("File already exists!...Try again")
+                else:
+                    error = False
+        else:
+            return
+    elif choice == "q":
+        return
+    
     else:
         print("MAJOR ERROR!!!")
 
 if __name__ == "__main__":
     ConsoleFront()
-
