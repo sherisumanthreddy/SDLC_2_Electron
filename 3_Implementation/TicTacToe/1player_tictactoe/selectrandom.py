7a39c7e12aa87177af53687d89f3992c2495cba7
Swathi Ramachandran <66193894+Swathir1@users.noreply.github.com>
2021-08-22 17:00:39 +0530

    Pytest

--- 3_Implementation/TicTacToe/1player_tictactoe/selectrandom.py
+++ 3_Implementation/TicTacToe/1player_tictactoe/selectrandom.py
@@ -1,6 +1,6 @@
 import random
 def select_random(list_num):
-    """Function"""
+    """Function to select random number"""
     length = len(list_num)
     r_int = random.randrange(0, length)
     return list_num[r_int]
