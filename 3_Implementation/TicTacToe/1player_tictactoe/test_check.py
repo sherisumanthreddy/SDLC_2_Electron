ccb19b6281ecc45010ec33902cda9ceee5f1e8a9
Swathi Ramachandran <66193894+Swathir1@users.noreply.github.com>
2021-08-22 16:20:03 +0530

    Add files via upload

---
+++ 3_Implementation/TicTacToe/1player_tictactoe/test_check.py
@@ -0,0 +1,8 @@
+import check
+
+b = [' ' for x in range(10)]
+B = ['x' for x in range(10)]
+
+def test_is_board_full():
+    assert check.is_board_full(b) == False
+    assert check.is_board_full(B) == True
