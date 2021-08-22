549062028e14908bbe161759e1010acd13439969
Swathi Ramachandran <66193894+Swathir1@users.noreply.github.com>
2021-08-22 16:19:01 +0530

    Update check.py

--- 3_Implementation/TicTacToe/1player_tictactoe/check.py
+++ 3_Implementation/TicTacToe/1player_tictactoe/check.py
@@ -1,6 +1,6 @@
 def is_board_full(board):
     """Function to check whether board if full or not"""
-    if board.count(' ') != 1:
+    if board.count(' ') > 1:
         return False
     else:
         return True
