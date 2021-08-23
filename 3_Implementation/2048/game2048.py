752fc79ce97a69809ed8e499f185950bb6536a52
yasirfaizahmed <yasirfaizahmed.n@gmail.com>
2021-08-23 12:19:04 +0530

    added GUI to tictac and made the menu bud free and some more changes

--- 3_Implementation/2048/game2048.py
+++ 3_Implementation/2048/game2048.py
@@ -2,7 +2,7 @@
 import turtle
 import random
 import pickle
-
+import tic_tac_toe
 
 
 #our main grid 
@@ -34,6 +34,30 @@ values = [0,1,2,3,4,5,6,7,8]
 text_colors = {
 	0:"#006600", 1:"#3498DB", 2:"#996633", 3:"#993366", 4:"#5D3AF8", 5:"#22D133", 6:"#C055D4", 7:"#55D4CE", 8:"#0066FF" 
 }
+
+#setting uo the screen
+window = turtle.Screen()
+window.title("Mini arcade games")
+window.bgcolor("black")
+window.setup(width=450, height=600)
+window.tracer(0)
+
+#general purpose pen turtle
+pen = turtle.Turtle()
+pen.speed(0)
+pen.hideturtle()  # an invisible turtle for drawing buttons
+
+#some constants
+CURSOR_SIZE = 20
+FONT_SIZE = 16
+FONT = ('Arial', FONT_SIZE, 'bold')
+ARCADE_FONT = ('Arcade Interlaced', FONT_SIZE, 'bold')
+
+#some multifile bool flags
+TESTING = False
+IN_MENU = True
+TICTAC = False 
+
 
 
 ## 
@@ -67,9 +91,12 @@ def pick_random_tile():
 #  @details 
 #  
 def menu():
-	#turtle.Screen().clear()
+	global IN_MENU
+	IN_MENU = True	#in menu
+	
 	pen.penup()
 	pen.clear()
+	tic_tac_toe.new.pen.clear()
 	#pen.goto(100, 100)
 	window.bgcolor("black")
 	window.setup(width = 500, height=450)
@@ -89,19 +116,23 @@ def menu():
 	#pen.setx(pen.xcor()+300)
 	
 	turtle.bgpic("bg.png")
-	pen.forward(110)
-	pen.left(90)	#now torn 90dergrees clock wise 
-	pen.forward(30)
-	pen.left(90)
+	
+	#2048 game 
+	pen.goto(-100, -95)	#with reference to the drawn canvas
+	pen.write("2048 Game", font=ARCADE_FONT)
 	pen.penup()
-	pen.goto(7, 6)	#with reference to the drawn canvas
-	pen.setx(pen.xcor()-100)
-	pen.sety(pen.ycor()-100)
-	pen.write("2048 game", font=ARCADE_FONT)
-	pen.setx(pen.xcor()+100)
-	pen.sety(pen.ycor()+100)
+	
+	#tictac game
+	pen.goto(-140,-165)
+	pen.write("TicTacToe Game", font=ARCADE_FONT)
 	pen.penup()
-
+	
+	#sudoku game
+	pen.goto(-120, -130)
+	pen.write("Sudoku Game", font=ARCADE_FONT)
+	pen.penup()
+	
+	pen.goto(90, 90)	#back to default position
 
 ## 
 #  @brief displays the score
@@ -114,16 +145,16 @@ def draw_score():
 
 	pen.goto(-185, 150)
 	pen.color("black")
-	pen.write("SCORE: ", font=('Arcade Interlaced', 10))
+	pen.write("SCORE: ", font=('Arcade Interlaced', 12))
 	pen.goto(-95, 150)
-	pen.write(str(score), font=('Arcade Interlaced', 10))
+	pen.write(str(score), font=('Arcade Interlaced', 12))
 	pen.penup()
 	
-	pen.goto(-10, 150)
+	pen.goto(-40, 150)
 	pen.color("black")
-	pen.write("HIGHSCORE: ", font=('Arcade Interlaced', 10))
+	pen.write("HIGHSCORE: ", font=('Arcade Interlaced', 12))
 	pen.goto(120, 150)
-	pen.write(str(high_score), font=('Arcade Interlaced', 10))
+	pen.write(str(high_score), font=('Arcade Interlaced', 12))
 	pen.penup()
 	
 
@@ -137,15 +168,21 @@ def draw_score():
 #  @details 
 #  
 def draw_grid():
-
-	#turtle.Screen().clear()
+	IN_MENU = False	#not in menu anymore
+	
+	tic_tac_toe.new.pen.clear()
 	pen.clear()	#clear all the previously drawn stuff
 	MOVE = 60	#some distnace btw two blocks
 	board_x = 0	#indexes
 	board_y = 0	
-	#pen.setx(pen.xcor()-60)
-	#pen.sety(pen.ycor()+60)
-	pen.goto(-50, 80)	#statying in middle
+	
+	pen.goto(-90, -150)
+	pen.color("white")
+	pen.write("'E' for menu", font=('Arcade Interlaced', 12))
+	pen.penup()
+	
+	
+	pen.goto(-110, 110)	#statying in middle
 	
 	for row in gameboard:
 		board_y = 0
@@ -158,13 +195,13 @@ def draw_grid():
 				pen.right(90)
 			pen.end_fill()	#boundary complete, fill the color
 			
-			pen.goto(pen.xcor()-27, pen.ycor()+10)	#going inside to enter the value of the gameboard[x][y] in middle of the blocks
+			pen.goto(pen.xcor()+25, pen.ycor()-35)	#going inside to enter the value of the gameboard[x][y] in middle of the blocks
 			pen.color("black")
 			number = gameboard[board_x][board_y]
 			if number == 0:	#if 0? leave empty string
 				number = ""
 			pen.write(str(number), align="center", font=FONT)
-			pen.goto(pen.xcor()+27, pen.ycor()-10)
+			pen.goto(pen.xcor()-25, pen.ycor()+35)
 			
 			board_y += 1
 			pen.setx(pen.xcor()+MOVE)	#increment 1 block of distance to make another one next to it
@@ -174,7 +211,8 @@ def draw_grid():
 		board_x +=1
 		pen.penup()	#finish writing
 		#_x += 60
-	
+		
+
 	draw_score()
 
 ## 
@@ -186,18 +224,64 @@ def draw_grid():
 #  
 #  @details 
 #  
-def btnclick_2048(x, y):
-	if x > -93 and x < 81 and y > -94 and y < -64:	#clicked properly?
-		#pen.clear()	#clearing the buttons from menu
+def btnclick(x, y):
+	global IN_MENU
+	global TICTAC
+	#2048
+	#print(x,y)
+	if (x > -93) and (x < 81) and (y > -94) and (y < -64) and IN_MENU:	#clicked at 2048?
 		window.bgcolor("black")
-		window.setup(width = 400, height=400)
+		window.setup(width=400, height=400)
 		window.title("2048")
-		
+		IN_MENU = False	#not in menu anymore
 		
 		pen.clear()	#just clearing in case
 		pick_random_tile()
 		pick_random_tile()
 		draw_grid()	#lets game!
+		
+	#tictactoe
+	if (x > -139) and (x < 100) and (y > -174) and (y < -140) and IN_MENU:
+		window.bgcolor("black")
+		window.setup(width=400, height=400)
+		window.title("TicTacToe")
+		
+		IN_MENU = False	#not in menu anymore
+		TICTAC = True 	# we are in TicTacToe! 
+		
+		pen.clear()	#just clearing in case
+		
+		tic_tac_toe.draw_tictac_grid()
+	
+	#tictactoe clicks
+	#global TIC_MOVE
+	if (x > -129) and (x < -60) and (y < 120) and (y > 50) and TICTAC:
+		tic_tac_toe.draw_tictac_grid(1)
+	elif (x > -44) and (x < 27) and (y < 120) and (y > 50) and TICTAC:
+		tic_tac_toe.draw_tictac_grid(2)
+	elif (x > 45) and (x < 117) and (y < 120) and (y > 50) and TICTAC:
+		tic_tac_toe.draw_tictac_grid(3)
+	elif (x > -129) and (x < -60) and (y < 36) and (y > -36) and TICTAC:
+		tic_tac_toe.draw_tictac_grid(4)
+	elif (x > -45) and (x < 30) and (y < 36) and (y > -36) and TICTAC:
+		tic_tac_toe.draw_tictac_grid(5)
+	elif (x > 45) and (x < 120) and (y < 36) and (y > -36) and TICTAC:
+		tic_tac_toe.draw_tictac_grid(6)
+	elif (x > -129) and (x < -60) and (y < -54) and (y > -129) and TICTAC:
+		tic_tac_toe.draw_tictac_grid(7)
+	elif (x > -45) and (x < 27) and (y < -54) and (y > -129) and TICTAC:
+		tic_tac_toe.draw_tictac_grid(8)
+	elif (x > 45) and (x < 117) and (y < -54) and (y > -129) and TICTAC:
+		tic_tac_toe.draw_tictac_grid(9)
+	
+		
+	
+	if (x > -139) and (x < 100) and (y > -129) and (y < -100) and IN_MENU:
+		
+		
+		IN_MENU = False	#not in menu anymore
+	
+		
 	
 #
 #  @brief Transpose of grid
@@ -352,7 +436,7 @@ def main():
 	window.onkeyrelease(move_up, "w")	#move up
 	window.onkeyrelease(move_down, "s")	#move down
 	
-	turtle.onscreenclick(btnclick_2048, 1)	#if mouse is clicked? check weather its inside the button's boundaries
+	turtle.onscreenclick(btnclick, 1)	#if mouse is clicked? check weather its inside the button's boundaries
 	window.mainloop()	#run the windown thread parallely
 
 if __name__ == "__main__":	
@@ -364,7 +448,6 @@ if __name__ == "__main__":
         high_score_pickle = open('highscorepickle.pkl', 'rb')
         h = pickle.load(high_score_pickle)
         high_score = h['score']
-        print(high_score)
         high_score_pickle.close()
 
     except FileNotFoundError:
@@ -375,30 +458,16 @@ if __name__ == "__main__":
         pickle.dump(h, high_score_pickle)
         high_score_pickle.close()
     
-    print('high score is: ' + str(high_score))
+    print('high score 2048 is: ' + str(high_score))
     score = 0
 
 
 
 
-# Seting up the screen
-    window = turtle.Screen()
-    window.title("Mini arcade games")
-    window.bgcolor("black")
-    window.setup(width=450, height=600)
-    window.tracer(0)
+# Setting up the screen
+    
 
-    #general purpose pen turtle
-    pen = turtle.Turtle()
-    pen.speed(0)
-    pen.hideturtle()  # an invisible turtle for drawing buttons
-
-    #some constants
-    CURSOR_SIZE = 20
-    FONT_SIZE = 15
-    FONT = ('Arial', FONT_SIZE, 'bold')
-    ARCADE_FONT = ('Arcade Interlaced', FONT_SIZE, 'bold')
-    TESTING = False
+    
     
     
 	
