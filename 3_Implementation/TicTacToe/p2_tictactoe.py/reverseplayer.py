a829349992e8d080e196941c8dc71e2c0b10c03c
Prakhar Deep <prakhardeep854@gmail.com>
2021-08-23 20:06:02 +0530

    Add files via upload

---
+++ 3_Implementation/TicTacToe/p2_tictactoe.py/reverseplayer.py
@@ -0,0 +1,13 @@
+from . import check
+# Flip the current player from X to O, or O to X
+
+def reverse_player():
+  # Global variables we need
+  
+  global first_player
+  # If the current player was X, make it O
+  if check.first_player == "X":
+    check.first_player = "O"
+  # Or if the current player was O, make it X
+  elif check.first_player == "O":
+    check.first_player = "X"
---
+++ 3_Implementation/TicTacToe/p2_tictactoe.py/takeinput.py
@@ -0,0 +1,3 @@
+def take_input():
+  x = input(" Do you want to play again?(y/n) : ")
+  return x
