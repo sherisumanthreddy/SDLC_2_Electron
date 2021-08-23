3c9ecfddad5103f24be1731b3d600587287fd6c2
Swathi Ramachandran <66193894+Swathir1@users.noreply.github.com>
2021-08-23 22:12:29 +0530

    code quality

--- 3_Implementation/TicTacToe/p1_tictactoe/test_spacefree.py
+++ 3_Implementation/TicTacToe/p1_tictactoe/test_spacefree.py
@@ -5,5 +5,5 @@ B = [' ', ' ', 'x', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
 
 def test_space_is_free():
-    assert spacefree.space_is_free(b, 2) == True
-    assert spacefree.space_is_free(B, 2) == False
+    assert spacefree.space_is_free(b, 2) == 1
+    assert spacefree.space_is_free(B, 2) == 0
