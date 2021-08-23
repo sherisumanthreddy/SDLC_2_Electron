54e3b8ff8f6f95dd1e34d07993adb47812d102d3
Swathi Ramachandran <66193894+Swathir1@users.noreply.github.com>
2021-08-23 14:43:11 +0530

    Update main.py

--- 3_Implementation/main.py
+++ 3_Implementation/main.py
@@ -1,4 +1,5 @@
 from SudokuCompanion import sudoku_companion_main
+from TicTacToe.p1_tictactoe import one_player_ttt
 
 if __name__ == "__main__":
     print("Welcome To MiniArcade Game!")
@@ -6,12 +7,15 @@ if __name__ == "__main__":
     print("Select from the following:")
     print("1. 2048 (Gui)")
     print("2. Sudoku Companion (Console)")
-    print("3. Tic-Tac-Toe")
+    print("3. Tic-Tac-Toe - 1 Player (Console)")
+    print("4. Tic-Tac-Toe - 2 player (Console)")
 
-    console_or_gui = input("Enter your selection [1, 2, 3] or q to quit: ")
+    console_or_gui = input("Enter your selection [1, 2, 3, 4] or q to quit: ")
     if console_or_gui == '1':
         print("This should launch gui")
     elif console_or_gui == '2':
         sudoku_companion_main.ConsoleFront()
+    elif console_or_gui == '3':
+        one_player_ttt.ConsoleFront()
     else:
         print("EXITING")
