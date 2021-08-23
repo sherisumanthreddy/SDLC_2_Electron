c9f4795c5e21da8e22ca4c61e8fa850fcd4177e4
Swathi Ramachandran <66193894+Swathir1@users.noreply.github.com>
2021-08-23 22:28:00 +0530

    code quality improved for 1 player tictactoe

--- 3_Implementation/TicTacToe/p1_tictactoe/test_check.py
+++ 3_Implementation/TicTacToe/p1_tictactoe/test_check.py
@@ -5,5 +5,5 @@ B = ['x' for x in range(10)]
 
 
 def test_is_board_full():
-    assert check.is_board_full(b) == False
-    assert check.is_board_full(B) == True
+    assert check.is_board_full(b) == 0
+    assert check.is_board_full(B) == 1
