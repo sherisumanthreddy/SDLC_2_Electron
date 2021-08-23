12f333d6065e9ea6cb4638910a09328eba93af31
yasirfaizahmed <yasirfaizahmed.n@gmail.com>
2021-08-23 17:13:48 +0530

    2 games working on GUI, nned to add sudoku as console game

--- 3_Implementation/2048/game2048.py
+++ 3_Implementation/2048/game2048.py
@@ -2,7 +2,7 @@
 import turtle
 import random
 import pickle
-import tic_tac_toe
+
 
 
 #our main grid 
@@ -13,6 +13,15 @@ gameboard = [
     [0, 0, 0, 0]
 ]
 
+TESTING = False
+
+if not TESTING:
+    import tic_tac_toe
+    window = turtle.Screen()
+    window.title("Mini arcade games")
+    window.bgcolor("black")
+    window.setup(width=450, height=600)
+    window.tracer(0)
 
 #grid colors
 colors = {
@@ -35,29 +44,10 @@ text_colors = {
 	0:"#006600", 1:"#3498DB", 2:"#996633", 3:"#993366", 4:"#5D3AF8", 5:"#22D133", 6:"#C055D4", 7:"#55D4CE", 8:"#0066FF" 
 }
 
-#setting uo the screen
-window = turtle.Screen()
-window.title("Mini arcade games")
-window.bgcolor("black")
-window.setup(width=450, height=600)
-window.tracer(0)
-
 #general purpose pen turtle
 pen = turtle.Turtle()
 pen.speed(0)
-pen.hideturtle()  # an invisible turtle for drawing buttons
-
-#some constants
-CURSOR_SIZE = 20
-FONT_SIZE = 16
-FONT = ('Arial', FONT_SIZE, 'bold')
-ARCADE_FONT = ('Arcade Interlaced', FONT_SIZE, 'bold')
-
-#some multifile bool flags
-TESTING = False
-IN_MENU = True
-TICTAC = False 
-
+pen.hideturtle()
 
 
 ## 
@@ -420,6 +410,7 @@ def move_down(TESTING = False):
 
 
 
+
 ## 
 #  @brief main
 #  
@@ -440,7 +431,28 @@ def main():
 	turtle.onscreenclick(btnclick, 1)	#if mouse is clicked? check weather its inside the button's boundaries
 	window.mainloop()	#run the windown thread parallely
 
+
+
 if __name__ == "__main__":	
+
+
+        #setting uo the screen
+
+
+ # an invisible turtle for drawing buttons
+
+    #some constants
+    CURSOR_SIZE = 20
+    FONT_SIZE = 16
+    FONT = ('Arial', FONT_SIZE, 'bold')
+    ARCADE_FONT = ('Arcade Interlaced', FONT_SIZE, 'bold')
+
+    #some multifile bool flags
+
+    IN_MENU = True
+    TICTAC = False
+
+
 
     global high_score
     global score
@@ -462,15 +474,5 @@ if __name__ == "__main__":
     if not TESTING:
         print('high score 2048 is: ' + str(high_score))
     score = 0
-
-
-
-
-# Setting up the screen
-    
-
-    
-    
-    
 	
     main()
